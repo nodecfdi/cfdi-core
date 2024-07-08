14ba6153ae33b2ab7dbc901b678af30adc5603f8
Fernando Isidro Luna <luffynando@gmail.com>
2024-07-08 17:26:14 -0600

    build: :wrench: update workflow for generate publish apps remove dev ref

--- index.ts
+++
@@ -1,9 +0,0 @@
-export * from './src/dom.js';
-export * from './src/utils/xml.js';
-export { default as XmlAttributes } from './src/xml_nodes/xml_attributes.js';
-export { default as XmlNode } from './src/xml_nodes/xml_node.js';
-export { default as XmlNodeExporter } from './src/xml_nodes/xml_node_exporter.js';
-export { default as XmlNodeImporter } from './src/xml_nodes/xml_node_importer.js';
-export * from './src/xml_nodes/xml_node_utils.js';
-export { default as XmlNodes } from './src/xml_nodes/xml_nodes.js';
-export { default as XmlNodesSorter } from './src/xml_nodes/xml_nodes_sorter.js';
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * from './dom.js';
+export * from './utils/xml.js';
+export { default as XmlAttributes } from './xml_nodes/xml_attributes.js';
+export { default as XmlNode } from './xml_nodes/xml_node.js';
+export { default as XmlNodeExporter } from './xml_nodes/xml_node_exporter.js';
+export { default as XmlNodeImporter } from './xml_nodes/xml_node_importer.js';
+export * from './xml_nodes/xml_node_utils.js';
+export { default as XmlNodes } from './xml_nodes/xml_nodes.js';
+export { default as XmlNodesSorter } from './xml_nodes/xml_nodes_sorter.js';
